272e8435947cf48cd9db27175f3c953c59681916
Logan <arechery.a1111@gmail.com>
2022-04-12 17:24:21 +0800

    Create Current Time

---
+++ HelloWorldDemo_WPF_Framework/CurrentTimeFolder/CT.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace HelloWorldDemo_WPF_Framework.CurrentTimeFolder {
+  /// <summary>
+  /// CT.xaml 的互動邏輯
+  /// </summary>
+  public partial class CT : UserControl {
+    DispatcherTimer Timer { get; set; }
+    public CT() {
+      InitializeComponent();
+      Timer = new DispatcherTimer();
+      Timer.Tick += CurrentTime;
+      Timer.Start();
+    }
+
+    private void CurrentTime(object sender, EventArgs e) =>
+      Print.Content = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss");
+  }
+}
