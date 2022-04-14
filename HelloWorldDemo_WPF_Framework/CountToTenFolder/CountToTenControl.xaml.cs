dd405c2857b2de3c0df7c9a6e156c907af26fe43
Logan <arechery.a1111@gmail.com>
2022-04-14 13:20:58 +0800

    Count To Ten Without Timer, Using Thread With Sleep

--- HelloWorldDemo_WPF_Framework/CountToTenFolder/CountToTenControl.xaml.cs
+++ HelloWorldDemo_WPF_Framework/CountToTenFolder/CountToTenControl.xaml.cs
@@ -1,17 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Diagnostics;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
 
 namespace HelloWorldDemo_WPF_Framework.CountToTenFolder {
@@ -22,32 +14,33 @@ namespace HelloWorldDemo_WPF_Framework.CountToTenFolder {
     public CountToTenControl() {
       InitializeComponent();
     }
-    // declare
-    private DispatcherTimer Timer { get; set; }
-    private int Duration { get; set; }
-    // CoutUp Function 
+    public int Duration { get; set; }
+    /// <summary>
+    /// CoutUp Function
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     private void CountUp(object sender, RoutedEventArgs e) {
-      // instance
-      Timer = new DispatcherTimer();
+      // show the first number to PrintLabel.Content
       Duration = 0;
       PrintLabel.Content = Duration.ToString();
-      // subscripttion Count_Up function
-#pragma warning disable CS8622 // 參數類型中參考型別是否可為 Null 的情況，與目標委派不相符 (可能的原因是屬性可為 Null)。
-      Timer.Tick += Count_Up;
-#pragma warning restore CS8622 // 參數類型中參考型別是否可為 Null 的情況，與目標委派不相符 (可能的原因是屬性可為 Null)。
-      // set interval
-      Timer.Interval = new TimeSpan(0, 0, 1);
-      // start the timer
-      Timer.Start();
+      // create sleep Thread with Sleep method
+      Thread sleepthread = new Thread(Sleep);
+      // wait 1 second
+      sleepthread.Start(1);
     }
-
-    // make an eventer
-    private void Count_Up(object sender, EventArgs e) {
-      if (Duration == 10) {
-        Timer.Stop();
-      } else if (Duration < 10) {
-        Duration += 1;
-        PrintLabel.Content = Duration.ToString();
+    /// <summary>
+    /// sleep method
+    /// </summary>
+    /// <param name="i"></param>
+    private void Sleep(object i) {
+      // make a loop with Thread.Sleep,Duration++ and PrintLabel.Content
+      for (int j = 0; j < 10; j++) {
+        Thread.Sleep((int)i * 1000);
+        Duration++;
+        Dispatcher.Invoke(() => {
+          PrintLabel.Content = Duration.ToString();
+        });
       }
     }
   }
